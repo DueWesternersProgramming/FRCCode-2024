d1c86c295e2c78d027fe8030020ad65797514a06
DueWesternersProgramming <frcprogrammers@duewestrobotics.org>
2024-02-05 12:38:09 -0500

    Added photonLib and setup simple vision subsystem

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class VisionSubsystem extends SubsystemBase{
+    PhotonCamera camera = new PhotonCamera("photonvision");
+
+    public VisionSubsystem(){
+
+    }
+
+    public void setPipeline(int index){
+        camera.setPipelineIndex(index);
+    }
+
+    public PhotonPipelineResult getResult(){
+        return camera.getLatestResult();
+    }
+
+    public boolean isCameraConnected(){
+        return camera.isConnected();
+    }
+    
+    public boolean hasResults() {
+        return camera.getLatestResult().hasTargets();
+    }
+
+    public PhotonTrackedTarget getBestTarget(){
+        return getResult().getBestTarget();
+    }
+    public double getTargetYaw(){
+        return getBestTarget().getYaw();
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putBoolean("Has a tracked object:", hasResults());
+    }
+}
