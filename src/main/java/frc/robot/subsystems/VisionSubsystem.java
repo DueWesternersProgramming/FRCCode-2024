4a2173c7796b9b289fb7788720e26d69d96455d6
DueWesternersProgramming <frcprogrammers@duewestrobotics.org>
2024-02-05 20:24:08 -0500

    small changes

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,17 +1,33 @@
 package frc.robot.subsystems;
+import frc.robot.Robot;
 import frc.robot.RobotConstants.VisionConstants;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class VisionSubsystem extends SubsystemBase{
-    PhotonCamera camera = new PhotonCamera(VisionConstants.PHOTON_CAMERA_NAME);
-
+    private PhotonCamera camera;
     public VisionSubsystem(){
-
+        try {
+            camera = new PhotonCamera(VisionConstants.PHOTON_CAMERA_NAME);
+        }
+        catch (NullPointerException e) {
+            System.out.println("Vision subsytem error:"+e);
+        }
     }
+
+    public int getAprilTagID() {
+        return getBestTarget().getFiducialId();
+    }
+
+    // private static boolean isValidTagId(int tagId) {
+    //     return tagId > 0 && tagId < 9;
+    // }
 
     public void setPipeline(int index){
         camera.setPipelineIndex(index);
@@ -23,14 +39,16 @@ public class VisionSubsystem extends SubsystemBase{
 
     public boolean isCameraConnected(){
         return camera.isConnected();
+        
     }
     
     public boolean hasResults() {
         return getResult().hasTargets();
     }
-
     public PhotonTrackedTarget getBestTarget(){
+
         return getResult().getBestTarget();
+        
     }
     public double getTargetYaw(){
         return getBestTarget().getYaw();
