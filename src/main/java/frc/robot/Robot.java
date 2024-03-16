d92abac60ed455e94eaf310493515fef411b31c6
DueWesternersProgramming <72952036+DueWesternersProgramming@users.noreply.github.com>
2024-03-16 11:33:40 -0400

    Added Deps Found Errors (Not Fixed)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -43,7 +43,6 @@ public class Robot extends LoggedRobot {
             Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
         }
 
-        // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
         Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
     }
 
